bda5d6815ef159eb51f513855f34802865f94262
Bo-Xuan Fan <coffee0127@gmail.com>
2016-09-16 12:05:22 +0800

    自訂 Pipe 元件

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+export * from './/my-pipe.pipe';
 export * from './car-detail';
 export * from './car-record';
 export * from './listbox';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavComponent } from './nav/nav.component';
 import { ListboxComponent } from './listbox/listbox.component';
 import { CarDetailComponent } from './car-detail/car-detail.component';
 import { CarRecordComponent } from './car-record/car-record.component';
+import { MyPipePipe } from './my-pipe.pipe';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CarRecordComponent } from './car-record/car-record.component';
     NavComponent,
     ListboxComponent,
     CarDetailComponent,
-    CarRecordComponent
+    CarRecordComponent,
+    MyPipePipe
   ],
   imports: [
     BrowserModule,
