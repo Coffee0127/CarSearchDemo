9a6baffac1adfb16cdea83d83a4a5764cfdf62ef
Bo-Xuan Fan <coffee0127@gmail.com>
2016-10-20 21:16:27 +0800

    內嵌繫結 ( interpolation )

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  projectName = 'Angular2 Car Search';
+
   constructor() { }
 
   ngOnInit() {
