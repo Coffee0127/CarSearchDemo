68d7a73b72183b042dd09c97bdc61284c0c65798
Bo-Xuan Fan <coffee0127@gmail.com>
2016-10-23 23:15:10 +0800

    add car-record component, which leads table fail

---
+++ src/app/car-record/car-record.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CarRecordComponent } from './car-record.component';
+
+describe('Component: CarRecord', () => {
+  it('should create an instance', () => {
+    let component = new CarRecordComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/car-record/car-record.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-car-record',
+  templateUrl: './car-record.component.html',
+  styleUrls: ['./car-record.component.css']
+})
+export class CarRecordComponent implements OnInit {
+
+  @Input()
+  i;
+
+  @Input()
+  car;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
